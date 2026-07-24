6aa5b56d20fda095c2aafd5e86decde23fe60005
Rap <96262034+Fancy-Raptor@users.noreply.github.com>
2026-07-24 00:24:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 import OBR from "https://esm.sh/@owlbear-rodeo/sdk";
 
+OBR.onReady(() => {
+  OBR.contextMenu.create({
+    id: "com.layer-toggler/toggle",
+    icons: [
+      {
+        icon: "./layers.svg",
+        label: "Toggle Prop / Attachment Layer",
+      }
+    ],
+    async onClick(context) {
+      const updates = context.items.map((item) => ({
+        id: item.id,
+        layer: item.layer === "PROP" ? "ATTACHMENT" : "PROP",
+      }));
+
+      await OBR.scene.items.updateItems(updates);
+    }
+  });
+});import OBR from "https://esm.sh/@owlbear-rodeo/sdk";
+
 OBR.onReady(async () => {
   // 1. Create the context menu button on selected items
   await OBR.contextMenu.create({
